be843914c3c3ba8c7aba2b5296b87013054a7010
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2024-12-03 16:00:12 +0100

    fix: mobile login button hidden

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -199,6 +199,7 @@ export default function HomePage({ user }: { user: User }): ReactNode {
                       initial={{ opacity: 0, y: -20 }}
                       animate={{ opacity: 1, y: 0 }}
                       transition={{ duration: 0.5, delay: 0.2 }}
+                      className="hidden lg:flex"
                     >
                       <Button onClick={() => router.push('/welcome')}>
                         <FormattedMessage
