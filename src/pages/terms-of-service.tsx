38952974f401f0d176d62542de697c74b4cc9fe4
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2024-12-08 15:29:57 +0100

    fix: unused import

--- src/pages/terms-of-service.tsx
+++ src/pages/terms-of-service.tsx
@@ -1,6 +1,6 @@
 import { PersonIcon } from '@radix-ui/react-icons'
 import Head from 'next/head'
-import router, { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import type { ReactNode } from 'react'
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl'
 
