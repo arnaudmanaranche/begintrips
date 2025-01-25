07cccdb157866f35864a070004f3d651a534f46d
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2025-01-25 15:27:46 +0100

    fix: svg warnings

--- src/components/MyJourneys/MyJourneys.tsx
+++ src/components/MyJourneys/MyJourneys.tsx
@@ -88,9 +88,9 @@ export default function MyJourneys({
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 className="h-5 w-5"
               >
                 <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"></path>
